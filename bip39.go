07381a12da26c5932bfa326f66f10779718fb0db
m1ngshum <140998506+m1ngshum@users.noreply.github.com>
2026-01-19 21:41:03 +0800

    Change module path to github.com/mocachain/go-bip39

--- bip39.go
+++ bip39.go
@@ -14,7 +14,7 @@ import (
 	"math/big"
 	"strings"
 
-	"github.com/tyler-smith/go-bip39/wordlists"
+	"github.com/mocachain/go-bip39/wordlists"
 	"golang.org/x/crypto/pbkdf2"
 )
 
--- bip39_test.go
+++ bip39_test.go
@@ -5,7 +5,7 @@ import (
 	"encoding/hex"
 	"testing"
 
-	"github.com/tyler-smith/go-bip39/wordlists"
+	"github.com/mocachain/go-bip39/wordlists"
 )
 
 type vector struct {
